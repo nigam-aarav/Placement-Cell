3be3637bda7d714a06afc5db18ea29a9dc84550e
nigam-aarav <145479717+nigam-aarav@users.noreply.github.com>
2023-12-16 21:11:08 +0530

    Delete route/result.js

--- route/result.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const resultPage = require('../controller/result');
-router.get('/:id' , resultPage.resultPage);
-router.post('/update', resultPage.update)
-module.exports = router;
